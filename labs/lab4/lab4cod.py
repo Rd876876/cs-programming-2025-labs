1ebed2b3c1219fcd5eb5211c255e550ac7007f0e
Rd876876 <dj.vadyamba@gmail.com>
2025-10-23 20:24:21 +1000

    2

--- labs/lab4/lab4cod.py
+++ labs/lab4/lab4cod.py
@@ -1,31 +1,33 @@
-#cel = float (input("введите температуру: "))
-#if cel <= 20:
-#    print("кондиционер включен")
-#else:
-#    print("кондиционер выключен")
-#
-#month = int (input ("введите номер месяца: "))
-#if month == 12 or month == 1 or month == 2:
-#    x = "зима"
-#elif 3 <= month <= 5:
-#    x = "весна"
-#elif 6 <= month <= 8:
-#    x = "лето"
-#elif 9 <= month <= 11:
-#    x = "осень"
-#else:
-#    print("неверный номер месяца")
-#if 1 <= month <= 12:
-#    print (x)
-#else :
-#    print("неверный номер месяца")
+cel = float (input("введите температуру: "))
+if cel <= 20:
+    print("кондиционер включен")
+else:
+    print("кондиционер выключен")
+
+
+month = int (input ("введите номер месяца: "))
+if month == 12 or month == 1 or month == 2:
+    x = "зима"
+elif 3 <= month <= 5:
+    x = "весна"
+elif 6 <= month <= 8:
+    x = "лето"
+elif 9 <= month <= 11:
+    x = "осень"
+else:
+    print("неверный номер месяца")
+if 1 <= month <= 12:
+    print (x)
+else :
+    print("неверный номер месяца")
+
+
 def age1(dog_age):
     if dog_age <= 2:
         human_age = dog_age * 10.5
     else:
         human_age = 21 + (dog_age - 2) * 4
     return human_age
-
 x = input("Введите возраст собаки (в годах): ")
 try:
     age = float(x)
@@ -38,9 +40,5 @@ try:
         print(f"Возраст собаки в человеческих годах: {human}")
 except ValueError:
     print("Ошибка: введено не число")
-
-
-
-
 
 
